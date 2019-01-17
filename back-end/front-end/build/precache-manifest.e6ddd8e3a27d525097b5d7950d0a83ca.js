d796fc86a6e368d3d5f8b67ce5d4c38c35124c2d
David Gleason <dgleason1990@gmail.com>
2019-01-17 17:02:34 -0500

    removed .gitignore

---
+++ back-end/front-end/build/precache-manifest.e6ddd8e3a27d525097b5d7950d0a83ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c8cc599a2f6165152f2",
+    "url": "/static/js/main.3c8cc599.chunk.js"
+  },
+  {
+    "revision": "0a51b696ff4597e6a1a4",
+    "url": "/static/js/1.0a51b696.chunk.js"
+  },
+  {
+    "revision": "3c8cc599a2f6165152f2",
+    "url": "/static/css/main.8423c11a.chunk.css"
+  },
+  {
+    "revision": "0a51b696ff4597e6a1a4",
+    "url": "/static/css/1.0c2ca21f.chunk.css"
+  },
+  {
+    "revision": "027841b4b2aa29064b6007ce7f913bff",
+    "url": "/index.html"
+  }
+];
